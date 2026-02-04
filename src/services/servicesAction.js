076efdef1d16eb2ff5198f364a34ac43bb2e477d
Rafi570 <hasan.cse570@gmail.com>
2026-02-04 19:25:43 +0600

    fetch cache done

--- src/services/servicesAction.js
+++ src/services/servicesAction.js
@@ -12,18 +12,21 @@ export const getSingleService = async (id) => {
   return res.json();
 };
 
+
+
+
+
 export const getAllServices = async (searchParams) => {
   const getParams = new URLSearchParams(searchParams).toString();
   console.log(getParams);
 
   const res = await fetch(
-    `https://car-washing-system-cleanify-server.vercel.app/api/v1/services?${getParams}`
+    `https://car-washing-system-cleanify-server.vercel.app/api/v1/services?${getParams}`,
+    {
+      cache: "force-cache",
+    }
   );
-  // await new Promise((resolve) =>
-  //   setTimeout(() => {
-  //     resolve();
-  //   }, 3000)
-  // );
+
   const data = await res.json();
   return data;
 };
